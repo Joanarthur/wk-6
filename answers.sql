9d13dcdb921eee6133a9f86c87b5fed86ca7c6ed
Joan Arthur <kahuaj2@gmail.com>
2025-09-08 12:57:03 +0300

    answers.sql

--- answers.sql
+++ answers.sql
@@ -1,3 +1,4 @@
+--Question 1
 USE salesdb;
 SELECT e.firstName, 
        e.lastName, 
@@ -6,3 +7,13 @@ SELECT e.firstName,
 FROM employees e
 INNER JOIN offices o
     ON e.officeCode = o.officeCode;
+
+--Question 2
+SELECT p.productName, 
+       p.productVendor, 
+       pl.productLine
+FROM products p
+LEFT JOIN productlines pl
+    ON p.productLine = pl.productLine;
+
+--Question 3
